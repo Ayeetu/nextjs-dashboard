203ce370d2435af07be042ce4cba814b9ae40663
Ayeetu <antoniosmoljann@gmail.com>
2024-02-02 16:37:20 +0100

    Added dashboard page with dynamic components

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,19 @@
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+
 export default function page() {
-  return <h1>Invoices page!</h1>;
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className}`}>Invoices</h1>
+      </div>
+      <div className="flex-center mt-4 flex justify-between gap-2 md:mt-8"></div>
+      <div className="mt-5 flex w-full justify-center"></div>
+    </div>
+  );
 }
